3f60651de729c91aedac4ec1a96e8523d7f9f050
Sacha Ponniah <sponniah812@gmail.com>
2021-11-16 14:09:07 +0000

    first bust_stop test passed

--- src/bus_stop.py
+++ src/bus_stop.py
@@ -1,2 +1,10 @@
 class BusStop:
+    def __init__(self, name):
+        self.name = name
+        self.queue = []
+
+    #def add_to_q(self, person):
+     #   self.queue.append(person)
+
+    
     
